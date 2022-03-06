d959a28a8e6dd17f04954fcec6cf553f81971493
jefferson de alcantara moura <jefferson22alcantara@gmail.com>
2022-03-06 20:46:01 -0300

    change-values

--- main.tf
+++ main.tf
@@ -13,3 +13,24 @@ module "aft" {
   global_customizations_repo_name               = "${var.github_username}/learn-terraform-aft-global-customizations"
   account_customizations_repo_name              = "${var.github_username}/learn-terraform-aft-account-customizations"
 }
+
+# module "aft" {
+#   source = "git@github.com:aws-ia/terraform-aws-control_tower_account_factory.git"
+
+#   # Required Parameters
+#   ct_management_account_id    = var.ct_management_account_id
+#   log_archive_account_id      = var.log_archive_account_id
+#   audit_account_id            = var.audit_account_id
+#   aft_management_account_id   = var.aft_management_account_id
+#   ct_home_region              = var.ct_home_region
+#   tf_backend_secondary_region = var.tf_backend_secondary_region
+
+#   # Optional Parameters
+#   terraform_distribution = "oss"
+#   vcs_provider           = "codecommit"
+
+#   # Optional Feature Flags
+#   aft_feature_delete_default_vpcs_enabled = false
+#   aft_feature_cloudtrail_data_events      = false
+#   aft_feature_enterprise_support          = false
+# }
